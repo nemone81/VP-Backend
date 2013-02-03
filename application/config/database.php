4f55c2aa7a14fc2fc416fcf66a2ee2ca78b6bab5
Fabio Crestoni <fabio.crestoni@gmail.com>
2013-02-03 15:14:31 +0100

    cambiati dati connessione

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'bwngxr';
-$db['default']['database'] = 'Sql460982_5';
+$db['default']['username'] = 'username';
+$db['default']['password'] = 'password';
+$db['default']['database'] = 'database';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
